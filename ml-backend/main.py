be52f0944a4db86b7b6dad36a63ca2d894f54dbd
sanjanahv <7sanjanarvce@gmail.com>
2026-04-04 19:31:18 +0530

    Add FastAPI app for plant disease prediction
    Implemented a FastAPI application for plant disease prediction using a pre-trained model. Added endpoints for home and prediction functionality.

--- ml-backend/main.py
+++ ml-backend/main.py
@@ -1,1 +1,46 @@
-hey
+from fastapi import FastAPI, UploadFile, File
+from tensorflow.keras.models import load_model
+import numpy as np
+from PIL import Image
+
+app = FastAPI()
+
+# load your FINAL model
+model = load_model("plant_disease_model.keras")
+
+class_names = [
+    'Tomato_Bacterial_spot',
+    'Tomato_Early_blight',
+    'Tomato_Late_blight',
+    'Tomato_Leaf_Mold',
+    'Tomato_Septoria_leaf_spot',
+    'Tomato_Spider_mites_Two_spotted_spider_mite',
+    'Tomato__Target_Spot',
+    'Tomato__Tomato_YellowLeaf__Curl_Virus',
+    'Tomato_healthy'
+]
+
+@app.get("/")
+def home():
+    return {"message": "Krushi Mitra ML API running"}
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    img = Image.open(file.file).resize((224,224))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    pred = model.predict(img_array)
+
+    prediction = class_names[np.argmax(pred)]
+    confidence = float(np.max(pred)) * 100
+
+    response = {
+        "prediction": prediction,
+        "confidence": confidence
+    }
+
+    if confidence < 50:
+        response["warning"] = "Low confidence prediction"
+
+    return response
